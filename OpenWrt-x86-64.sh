ebf70e829a42436838e9c268a43bcfa58a8f552c
sevilx <sevilxiong@gmail.com>
2020-02-11 15:16:38 +0800

    Update OpenWrt-x86-64.sh

--- OpenWrt-x86-64.sh
+++ OpenWrt-x86-64.sh
@@ -7,7 +7,7 @@
 # Blog: https://p3terx.com
 #=================================================
 
-# 定制默认IP
+# 定制默认IP,替换默认的192.168.1.1
 #sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 添加第三方软件包
