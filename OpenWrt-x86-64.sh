ea9cd10d7f211ad660286178cb8aa4e69367fc37
sevilx <sevilxiong@gmail.com>
2020-02-11 23:59:13 +0800

    Update OpenWrt-x86-64.sh

--- OpenWrt-x86-64.sh
+++ OpenWrt-x86-64.sh
@@ -10,8 +10,21 @@
 # 定制默认IP,替换默认的192.168.1.1
 #sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
+# Change timezone
+#cd ../..
+#sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+# Change default theme
+#sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
+
+# Remove bootstrap theme
+#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+#CONFIG_KERNEL_BUILD_USER="Compiled by Ben"
+#CONFIG_GRUB_TITLE="OpenWrt AutoBuild"
+
 # 添加第三方软件包
-git clone https://github.com/KFERMercer/luci-app-serverchan package/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/kang-mk/luci-app-smartinfo package/luci-app-smartinfo
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
 git clone https://github.com/Leo-Jo-My/luci-theme-leo.git package/luci-theme-leo
